7165e447fc4e0f2b86107824685695a43a9ecb26
Siddhant Pawar <79016942+justSid404@users.noreply.github.com>
2025-12-03 13:52:09 +0530

    Fixed an Interval/Timer issue
    Fixed an issue where Timers/Intervals were not terminated in some cases. Also Removed redundant console.log commands.

--- scripts/music-player.js
+++ scripts/music-player.js
@@ -214,6 +214,7 @@ function playMusic(songName) {
 
   playBtnElement.innerHTML = '<svg id="play-pause-pause-button-icon" class="media-control-button-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.163 3.819C5 4.139 5 4.559 5 5.4v13.2c0 .84 0 1.26.163 1.581a1.5 1.5 0 0 0 .656.655c.32.164.74.164 1.581.164h.2c.84 0 1.26 0 1.581-.163a1.5 1.5 0 0 0 .656-.656c.163-.32.163-.74.163-1.581V5.4c0-.84 0-1.26-.163-1.581a1.5 1.5 0 0 0-.656-.656C8.861 3 8.441 3 7.6 3h-.2c-.84 0-1.26 0-1.581.163a1.5 1.5 0 0 0-.656.656zm9 0C14 4.139 14 4.559 14 5.4v13.2c0 .84 0 1.26.164 1.581a1.5 1.5 0 0 0 .655.655c.32.164.74.164 1.581.164h.2c.84 0 1.26 0 1.581-.163a1.5 1.5 0 0 0 .655-.656c.164-.32.164-.74.164-1.581V5.4c0-.84 0-1.26-.163-1.581a1.5 1.5 0 0 0-.656-.656C17.861 3 17.441 3 16.6 3h-.2c-.84 0-1.26 0-1.581.163a1.5 1.5 0 0 0-.655.656z" fill="#ffffff"></path></g></svg>';
 
+  clearInterval(intervalId);
   setTimer();
 
 }
@@ -306,8 +307,6 @@ function shuffleArray(array) {
     [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
 
   }
-
-  console.log('array shuffled!');
 
 }
 
@@ -450,6 +449,7 @@ playBtnElement.addEventListener('click', () => {
 
     playBtnElement.innerHTML = '<svg id="play-pause-pause-button-icon" class="media-control-button-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.163 3.819C5 4.139 5 4.559 5 5.4v13.2c0 .84 0 1.26.163 1.581a1.5 1.5 0 0 0 .656.655c.32.164.74.164 1.581.164h.2c.84 0 1.26 0 1.581-.163a1.5 1.5 0 0 0 .656-.656c.163-.32.163-.74.163-1.581V5.4c0-.84 0-1.26-.163-1.581a1.5 1.5 0 0 0-.656-.656C8.861 3 8.441 3 7.6 3h-.2c-.84 0-1.26 0-1.581.163a1.5 1.5 0 0 0-.656.656zm9 0C14 4.139 14 4.559 14 5.4v13.2c0 .84 0 1.26.164 1.581a1.5 1.5 0 0 0 .655.655c.32.164.74.164 1.581.164h.2c.84 0 1.26 0 1.581-.163a1.5 1.5 0 0 0 .655-.656c.164-.32.164-.74.164-1.581V5.4c0-.84 0-1.26-.163-1.581a1.5 1.5 0 0 0-.656-.656C17.861 3 17.441 3 16.6 3h-.2c-.84 0-1.26 0-1.581.163a1.5 1.5 0 0 0-.655.656z" fill="#ffffff"></path></g></svg>';
 
+    clearInterval(intervalId);
     setTimer();
 
   }
@@ -461,8 +461,6 @@ nextBtnElement.addEventListener('click', async () => {
   if(shuffleBtnElement.dataset.shuffleState == 1) {
 
     shuffledSongList.forEach(async (songItem, index) => {
-
-      console.log('"'+shuffleSongIndex+'"');
 
       if(String(shuffleSongIndex) === "null") {
 
